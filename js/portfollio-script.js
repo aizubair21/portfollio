c114e136c035cdc1f2373ebdaac381db4182e495
Amimul ihsan <86657812+amimul21@users.noreply.github.com>
2022-01-13 02:41:34 +0600

    service section

--- js/portfollio-script.js
+++ js/portfollio-script.js
@@ -67,3 +67,125 @@ function closeNavButton() {
 }
 
 console.log(document.getElementsByClassName("nav_item"))
+
+$("#tsparticles")
+  .particles()
+  .init(
+    {
+      background: {
+        color: {
+          value:0,
+        },
+      },
+      fpsLimit: 100,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "grab",
+          },
+          resize: true,
+        },
+        modes: {
+          bubble: {
+            distance: 400,
+            duration: 2,
+            opacity: 0.8,
+            size: 40,
+          },
+          grab: {
+            distance: 150,
+            line_linked: {
+              opacity: 1
+            }
+          },
+          bubble: {
+            distance: 800,
+            size: 80,
+            duration: 2,
+            opacity: 0.8,
+            speed: 3
+          },
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#dd8d31",
+        },
+        links: {
+          color: "#dd8d31",
+          distance: 80,
+          enable:true,
+          opacity: 0.5,
+          width: 1,
+        },
+        collisions: {
+          enable: true,
+        },
+        move: {
+          direction: "none",//none, row, colums
+          enable: true,
+          outMode: "bounce",//bounce, 
+          random: false,
+          speed: 3,
+          straight: false,
+        },
+        // move: {
+        //   enable: true,
+        //   speed: 5,
+        //   direction: "none",
+        //   random: false,
+        //   straight: false,
+        //   out_mode: "out",
+        //   bounce: false,
+        //   attract: {
+        //     enable: false,
+        //     rotateX: 600,
+        //     rotateY: 1200
+        //   }
+        // },
+        number: {
+          density: {
+            enable: true,
+            area: 800,
+          },
+          value: 150,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "triangle",
+          /* rect, squre, circle, triangle, oval*/
+        },
+        size: {
+          random: true,
+          value: 5,
+        },
+      },
+      detectRetina: true,
+    },
+    function (container) {
+      // container is the particles container where you can play/pause or stop/start.
+      // the container is already started, you don't need to start it manually.
+    }
+  );
+// or
+
+$("#tsparticles")
+  .particles()
+  .ajax("particles.json", function (container) {
+    // container is the particles container where you can play/pause or stop/start.
+    // the container is already started, you don't need to start it manually.
+  });
