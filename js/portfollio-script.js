1c8d8c6ef1d6d65d96c3615599490890cfe9281a
Amimul ihsan <86657812+amimul21@users.noreply.github.com>
2022-01-12 20:37:23 +0600

    start with new design

--- js/portfollio-script.js
+++ js/portfollio-script.js
@@ -1,13 +1,12 @@
 
 $(function () {
-  $("#home").removeClass("go_to_back");
-  // $("#home").addClass("current_position");
+  // $("#home").removeClass("go_to_back");
+  // // $("#home").addClass("current_position");
 
   $(".nav-link").on("click", function (e) {
     e.preventDefault();
 
     $(".nav-link").removeClass("active");
-
     $(this).addClass("active");
 
   });
@@ -19,42 +18,52 @@ $(function () {
 
 });
 
-function openTabContent(tabContent) {
-    $(".tab-content").removeClass("current_position");
-    $(".tab-content").addClass("go_to_back");
-    let tab_content = $(".tab-content");
-    for (let index = 0; index <= tab_content.length; index++) {
-      const element = tab_content[index];
-      console.log(element);
+// function openTabContent(tabContent) {
+//     $(".tab-content").removeClass("current_position");
+//     $(".tab-content").addClass("go_to_back");
+//     let tab_content = $(".tab-content");
+//     for (let index = 0; index <= tab_content.length; index++) {
+//       const element = tab_content[index];
+//       console.log(element);
       
-    }
+//     }
 
-     if (tabContent == "home") {
-      $("#home").removeClass("go_to_back");
-      $("#home").addClass("current_position");
+//      if (tabContent == "home") {
+//       $("#home").removeClass("go_to_back");
+//       $("#home").addClass("current_position");
 
-     }else if (tabContent == "aboutUs") {
-      $("#aboutUs").removeClass("go_to_back");
-      $("#aboutUs").addClass("current_position");
+//      }else if (tabContent == "aboutUs") {
+//       $("#aboutUs").removeClass("go_to_back");
+//       $("#aboutUs").addClass("current_position");
 
-     }else if (tabContent == "portfollio") {
-      $("#portfollio").removeClass("go_to_back");
-      $("#portfollio").addClass("current_position");
+//      }else if (tabContent == "portfollio") {
+//       $("#portfollio").removeClass("go_to_back");
+//       $("#portfollio").addClass("current_position");
 
-     } else if (tabContent == "testimonial") {
-      $("#testimonial").removeClass("go_to_back");
-       $("#testimonial").addClass("current_position");
+//      } else if (tabContent == "testimonial") {
+//       $("#testimonial").removeClass("go_to_back");
+//        $("#testimonial").addClass("current_position");
 
-     } else if (tabContent == "contact") {
-      $("#contact").removeClass("go_to_back");
-       $("#contact").addClass("current_position");
+//      } else if (tabContent == "contact") {
+//       $("#contact").removeClass("go_to_back");
+//        $("#contact").addClass("current_position");
 
-     } else {
-      $("#home").css("display","block");
-     }
+//      } else {
+//       $("#home").css("display","block");
+//      }
 
      
-  };
+//   };
 
   function filterItem($elemeent_name) {
-  }
+  };
+
+function openNavButton () {
+  $(".nav_item").css({"left":"0px"});
+};
+
+function closeNavButton() {
+  $(".nav_item").css({"left":"-100%"})
+}
+
+console.log(document.getElementsByClassName("nav_item"))
