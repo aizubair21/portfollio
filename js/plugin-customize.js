06ad3ae7b407bd62519346df6683d08ee0d4d19c
Amimul ihsan <86657812+amimul21@users.noreply.github.com>
2022-01-26 09:08:47 +0600

    design update

--- js/plugin-customize.js
+++ js/plugin-customize.js
@@ -1,5 +1,98 @@
-
-
-(function(tsparticles){"use strict";$.fn.particles=function(){var _this=this;var baseId="tsparticles";var init=function init(options,callback){_this.each(function(index,element){if(element.id===undefined){element.id=baseId+Math.floor(Math.random()*1e3)}tsparticles.tsParticles.load(element.id,options).then(callback)})};var ajax=function ajax(jsonUrl,callback){_this.each(function(index,element){if(element.id===undefined){element.id=baseId+Math.floor(Math.random()*1e3)}tsparticles.tsParticles.loadJSON(element.id,jsonUrl).then(callback)})};return{init:init,ajax:ajax}}})(window);
 //# sourceMappingURL=jquery.particles.min.js.map
+(function(tsparticles){"use strict";$.fn.particles=function(){var _this=this;var baseId="tsparticles";var init=function init(options,callback){_this.each(function(index,element){if(element.id===undefined){element.id=baseId+Math.floor(Math.random()*1e3)}tsparticles.tsParticles.load(element.id,options).then(callback)})};var ajax=function ajax(jsonUrl,callback){_this.each(function(index,element){if(element.id===undefined){element.id=baseId+Math.floor(Math.random()*1e3)}tsparticles.tsParticles.loadJSON(element.id,jsonUrl).then(callback)})};return{init:init,ajax:ajax}}})(window);
+
+
+
+
+$("#particles-js")
+.particles()
+.init(
+  {
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,  
+          mode: "push",
+        },  
+        onHover: {
+          enable: true,  
+          mode: "grab",
+        },  
+        resize: true,
+      },  
+      modes: {
+        bubble: {
+          distance: 400,  
+          duration: 2,
+          opacity: 0.8,
+          size: 40,
+        },  
+        grab: {
+          distance: 150,  
+          line_linked: {
+            opacity: 1  
+          }  
+        },  
+        bubble: {
+          distance: 800,  
+          size: 80,
+          duration: 2,
+          opacity: 0.8,
+          speed: 3
+        },  
+        push: {
+          quantity: 4,  
+        },  
+        repulse: {
+          distance: 200,  
+          duration: 0.4,
+        },  
+      },  
+    },  
+    particles: {
+      color: {
+        value: "#dd8d31",  
+      },  
+      links: {
+        color: "#dd8d31",  
+        distance: 80,
+        enable:true,
+        opacity: 0.5,
+        width: 1,
+      },  
+      collisions: {
+        enable: true,  
+      },  
+      move: {
+        direction: "none",//none, row, colums  
+        enable: true,
+        outMode: "bounce",//bounce, 
+        random: false,
+        speed: 3,
+        straight: false,
+      },  
+      number: {
+        density: {
+          enable: true,  
+          area: 500,
+        },  
+        value: 80,
+      },  
+      opacity: {
+        value: 0.5,  
+      },  
+      shape: {
+        type: "triangle",  
+        /* rect, squre, circle, triangle, oval*/
+      },  
+      size: {
+        random: true,  
+        value: 5,
+      },  
+    },  
+    detectRetina: false,
+  },  
+
+);  
+
 
